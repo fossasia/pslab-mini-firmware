e9bdad6a292d7b1d02797bff0b4ae49e41190d25
Alexander Bessman <alexander.bessman@gmail.com>
2025-06-14 16:42:20 +0200

    Add UART/LED example
    Co-authored-by: Tejas Garg <tejasgarg8421@gmail.com>

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "stm32h5xx_hal_conf.h"
-
-int main(void)
-{
-  for (;;) {}
-  return 0;
-}
---
+++ src/system/led.h
@@ -0,0 +1,6 @@
+#ifndef SYSTEM_LED_H
+#define SYSTEM_LED_H
+
+void LED_toggle(void);
+
+#endif // SYSTEM_LED_H
