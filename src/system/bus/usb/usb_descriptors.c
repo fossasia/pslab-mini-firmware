459fd7ac1ebf11abedd3de90ba67a474a94166ca
Alexander Bessman <alexander.bessman@gmail.com>
2025-06-26 16:10:47 +0200

    Clean up usb_descriptors.c

--- src/system/bus/usb/usb_descriptors.c
+++ src/system/bus/usb/usb_descriptors.c
@@ -1,11 +1,37 @@
+/**
+ * @file usb_descriptors.c
+ * @brief USB descriptor definitions and TinyUSB callbacks.
+ * 
+ * USB descriptors are data structures used by USB devices to describe their
+ * identity, capabilities, and configuration to the host system. They include
+ * information such as device class, vendor and product IDs, supported
+ * configurations, and human-readable strings.
+ * 
+ * This file defines the USB device, configuration, and string descriptors for
+ * the device, and implements TinyUSB callbacks to integrate with the USB
+ * stack.
+ */
+
 #include <assert.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "tusb.h"
 
-#define LANGID ((const char[]){0x09, 0x04}) // English
-#define MANUSTR ("TinyUSB")
-#define PRODSTR ("STM32H5 CDC Demo")
-#define SERISTR ("123456")
+#include "usb.h"
+
+#define LANG ((char const[]){0x09, 0x04}) // English
+#define MANU ("FOSSASIA")
+#define PROD ("Pocket Science Lab")
+#define SERI (NULL) // Unique identifier, calculated at runtime from MCU UID.
+
+enum {
+    IDX_LANG,
+    IDX_MANU,
+    IDX_PROD,
+    IDX_SERI,
+    IDX_TOT
+};
 
 // Device
 tusb_desc_device_t const desc_device = {
@@ -16,8 +42,8 @@ tusb_desc_device_t const desc_device = {
     .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
     .bDeviceProtocol    = MISC_PROTOCOL_IAD,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-    .idVendor           = 0xCafe,
-    .idProduct          = 0x4001,
+    .idVendor           = 0xcafe,
+    .idProduct          = 0x1234,
     .bcdDevice          = 0x0100,
     .iManufacturer      = 0x01,
     .iProduct           = 0x02,
@@ -32,34 +58,47 @@ uint8_t const desc_configuration[] = {
 };
 
 // String descriptors
-char const* string_desc_arr[] = {
-    LANGID,
-    MANUSTR,
-    PRODSTR,
-    SERISTR
+char const *string_desc_arr[] = {
+    LANG,
+    MANU,
+    PROD,
+    SERI
 };
 
-uint8_t const* tud_descriptor_device_cb(void) { return (uint8_t const*)&desc_device; }
-uint8_t const* tud_descriptor_configuration_cb(uint8_t index) { (void)index; return desc_configuration; }
-uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+uint8_t const *tud_descriptor_device_cb(void) { return (uint8_t const *)&desc_device; }
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) { (void)index; return desc_configuration; }
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    if (index >= IDX_TOT) {return NULL;}
+
     (void)langid;
-    static uint16_t _desc_str[32];
+    static uint16_t _desc_str[32 + 1];
+    #define MAX_DESC_LEN (sizeof(_desc_str) / sizeof(_desc_str[0]) - 1)
 
-    static_assert((sizeof(LANGID) / sizeof(LANGID[0])) <= sizeof(_desc_str), "LANGID too long");
-    static_assert((sizeof(MANUSTR) / sizeof(MANUSTR[0])) <= sizeof(_desc_str), "MANUSTR too long");
-    static_assert((sizeof(PRODSTR) / sizeof(PRODSTR[0])) <= sizeof(_desc_str), "PRODSTR too long");
-    static_assert((sizeof(SERISTR) / sizeof(SERISTR[0])) <= sizeof(_desc_str), "SERISTR too long");
+    static_assert(sizeof(LANG) == 2, "LANG id must be exactly two bytes");
+    static_assert(sizeof(MANU) <= MAX_DESC_LEN, "MANU str too long");
+    static_assert(sizeof(PROD) <= MAX_DESC_LEN, "PROD str too long");
 
-    const char* str = string_desc_arr[index];
+    int len;
 
-    if (!str) return NULL;
-
-    const int len = strlen(str);
-    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2*len + 2);
-
-    for (int i = 0; i < len; i++) {
-        _desc_str[1+i] = str[i];
+    switch (index) {
+    case IDX_SERI:
+        len = USB_get_serial(_desc_str + 1, MAX_DESC_LEN);
+        break;
+    case IDX_LANG:
+        len = 1;
+        memcpy(_desc_str + 1, string_desc_arr[index], 2 * len);
+        break;
+    default:
+        char const *str = string_desc_arr[index];
+        len = strlen(str);
+        // Convert ASCII string into UTF-16
+        for (size_t i = 0; i < len; ++i) {
+            _desc_str[1 + i] = str[i];
+        }
+        break;
     }
+
+    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * len + 2);
 
     return _desc_str;
 }
