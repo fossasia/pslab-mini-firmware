692f1abe67643efca8480c0bdc96405c98319f1a
Alexander Bessman <alexander.bessman@gmail.com>
2025-08-25 12:52:16 +0200

    formatting

--- src/util/si_prefix.h
+++ src/util/si_prefix.h
@@ -2,8 +2,9 @@
  * @file si_prefix.h
  * @brief SI prefix constants for unit conversions
  *
- * This file provides constants for SI prefixes from tera (10^12) to pico (10^-12).
- * These constants can be used for unit conversions and scaling operations.
+ * This file provides constants for SI prefixes from tera (10^12) to pico
+ * (10^-12). These constants can be used for unit conversions and scaling
+ * operations.
  */
 
 #ifndef SI_PREFIX_H
@@ -16,28 +17,28 @@ extern "C" {
 /* Alternative integer-based constants for operations requiring larger ranges */
 
 /** Tera as integer constant */
-#define SI_TERA_INT     1000000000000ULL
+#define SI_TERA_INT 1000000000000ULL
 
 /** Giga as integer constant */
-#define SI_GIGA_INT     1000000000UL
+#define SI_GIGA_INT 1000000000UL
 
 /** Mega as integer constant */
-#define SI_MEGA_INT     1000000UL
+#define SI_MEGA_INT 1000000UL
 
 /** Kilo as integer constant */
-#define SI_KILO_INT     1000UL
+#define SI_KILO_INT 1000UL
 
 /** Milli as divisor (for multiplication) */
-#define SI_MILLI_DIV    1000UL
+#define SI_MILLI_DIV 1000UL
 
 /** Micro as divisor (for multiplication) */
-#define SI_MICRO_DIV    1000000UL
+#define SI_MICRO_DIV 1000000UL
 
 /** Nano as divisor (for multiplication) */
-#define SI_NANO_DIV     1000000000UL
+#define SI_NANO_DIV 1000000000UL
 
 /** Pico as divisor (for multiplication) */
-#define SI_PICO_DIV     1000000000000ULL
+#define SI_PICO_DIV 1000000000000ULL
 
 #ifdef __cplusplus
 }
