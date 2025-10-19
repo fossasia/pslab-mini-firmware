4264206bcb7a8ea80a5b5f05ed093f98f1efa867
Tejasgarg <Tejasgarg8421@gmail.com>
2025-10-19 08:41:01 +0000

    more fixes

--- src/platform/h563xx/led_ll.c
+++ src/platform/h563xx/led_ll.c
@@ -7,12 +7,10 @@
  *
  * Hardware Implementation Details:
  * - Uses STM32H5xx HAL library for GPIO operations
- * - LED_GREEN (LED1) is connected to GPIOB Pin 0 (PB0)
- * - LED_YELLOW (LED2) is connected to GPIOF Pin 4 (PF4)
- * - LED_RED (LED3) is connected to GPIOG Pin 4 (PG4)
+ * - LED_GREEN (LED1) is connected to GPIOB Pin 12 (PB12)
  * - Configured as push-pull output with no pull-up/pull-down resistors
  * - Low frequency GPIO speed setting for power efficiency
- * - Requires GPIOB, GPIOF, and GPIOG clocks to be enabled
+ * - Requires GPIOB clock to be enabled
  *
  * @author PSLab Team
  * @date 2025
@@ -25,12 +23,6 @@
 #define LED_GREEN_GPIO_GROUP GPIOB
 #define LED_GREEN_GPIO_PIN GPIO_PIN_12
 
-#define LED_YELLOW_GPIO_GROUP GPIOF
-#define LED_YELLOW_GPIO_PIN GPIO_PIN_4
-
-#define LED_RED_GPIO_GROUP GPIOG
-#define LED_RED_GPIO_PIN GPIO_PIN_4
-
 typedef struct {
     GPIO_TypeDef *gpio_group;
     uint16_t gpio_pin;
@@ -38,8 +30,6 @@ typedef struct {
 
 static LEDConfig const g_LED_CONFIGS[LED_LL_COUNT] = {
     [LED_LL_GREEN] = { LED_GREEN_GPIO_GROUP, LED_GREEN_GPIO_PIN },
-    [LED_LL_YELLOW] = { LED_YELLOW_GPIO_GROUP, LED_YELLOW_GPIO_PIN },
-    [LED_LL_RED] = { LED_RED_GPIO_GROUP, LED_RED_GPIO_PIN },
 };
 
 /**
@@ -55,19 +45,13 @@ void LED_LL_init(void)
 
     /* Enable GPIO port clocks */
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOF_CLK_ENABLE();
-    __HAL_RCC_GPIOG_CLK_ENABLE();
 
     /* Configure common GPIO settings */
     gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
     gpio_init.Pull = GPIO_NOPULL;
     gpio_init.Speed = GPIO_SPEED_FREQ_LOW;
-
-    /* Configure each LED pin */
-    for (int i = 0; i < LED_LL_COUNT; ++i) {
-        gpio_init.Pin = g_LED_CONFIGS[i].gpio_pin;
-        HAL_GPIO_Init(g_LED_CONFIGS[i].gpio_group, &gpio_init);
-    }
+    gpio_init.Pin = g_LED_CONFIGS[0].gpio_pin;
+    HAL_GPIO_Init(g_LED_CONFIGS[0].gpio_group, &gpio_init);
 }
 
 void LED_LL_on(LED_LL_ID led_id)
